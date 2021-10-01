053f5f6655153401909b4149279579f877453129
Diana Aguirre <a01276380@itesm.mx>
2021-10-01 03:23:12 -0500

    Se hacen cambios en js y hoja de estilos de componente para pruebas.

--- src/components/HeaderBusqueda.js
+++ src/components/HeaderBusqueda.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import '../styles/HeaderBusqueda.css';
 import '../styles/general.css';
+import '../styles/inputs.css';
+import '../styles/glass.css';
+import '../styles/botones.css';
 
 function HeaderBusqueda() {
     return (
-       <div className="headerBusqueda-container">
+       <div className="headerBusqueda-container lightGlass">
            <div className="header-left">
                 <h1 className="titulo-header">Titulo</h1>
-                <input type="text" className="input-busqueda" />
+                <input type="text" className="input-busqueda inputDarkGlass" />
            </div>
            <div className="header-right">
                 <div className="agregar-container">
                     <h3>Agregar</h3>
-                    <button className='btn-agregar'><i class="fas fa-plus"></i></button>
+                    <button className='btnMasGlass'><i class="fas fa-plus"></i></button>
                 </div>
                 <button className="btn-filtros">Filtros</button>
            </div>
