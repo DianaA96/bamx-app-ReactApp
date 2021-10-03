bd5f9ead53e02bc804215591dc6a1cf726eac941
Diana Aguirre <a01276380@itesm.mx>
2021-10-02 23:59:04 -0500

    Añade header simple de título

---
+++ src/components/HeaderTitulo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../styles/HeaderTitulo.css'
+import '../styles/general.css';
+import '../styles/glass.css';
+
+function HeaderTitulo() {
+    return (
+        <div className="headerTituloContainer lightGlass">
+            <h1 className="bebas1 blanco">
+                Agregar ruta
+            </h1>
+        </div>
+    );
+}
+
+export default HeaderTitulo;
