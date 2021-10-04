7c6c25f55f8c687cea3aa7aacfb7a34fdbe1e7e2
DianaA96 <69890392+DianaA96@users.noreply.github.com>
2021-10-04 00:42:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import './App.css';
-import HeaderBusquedaRutas from './components/FormularioAgregarRuta';
-
 
 function App() {
   return (
     <div className="App">
-      <HeaderBusquedaRutas></HeaderBusquedaRutas>
+      
     </div>
   );
 }
