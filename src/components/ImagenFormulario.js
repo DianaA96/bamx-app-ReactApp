34f2ffc927c198943d718620de5ee8ea177c83aa
Diana Aguirre <a01276380@itesm.mx>
2021-10-05 23:35:56 -0500

    Arregla problema con imagen de elotitos

--- src/components/ImagenFormulario.js
+++ src/components/ImagenFormulario.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import '../styles/ImagenFormulario.css';
-import imgVerduras from '../assets/images/verduras.png'
 
 function ImagenFormulario() {
     return (
         <div className="ImagenFormulario-container">
-            <img src={imgVerduras} alt="" className="img-formulario"/>
         </div>
     )
 }
