58972f27195d58534b1df65e4806e4028a45f153
Diana Aguirre <a01276380@itesm.mx>
2021-10-05 20:35:15 -0500

    Modifica estructura del header, pasa props

--- src/components/HeaderOperadorRuta.js
+++ src/components/HeaderOperadorRuta.js
@@ -3,12 +3,12 @@ import '../styles/HeaderOperadorRuta.css';
 import '../styles/general.css';
 import '../styles/glass.css';
 
-function HeaderOperadorRuta() {
+function HeaderOperadorRuta(props) {
     return (
         <div className="headerOperador-container darkGlass ">
-            <h1 className="titulo-header bebas1 blanco">Titulo</h1>
-            <p className="manrope2 bold">Nombre Operador</p>
-            <p className="manrope4">ID Operador</p>
+            <h1 className="titulo-header bebas1 blanco">Gestión de operadores en ruta</h1>
+            <p className="manrope2 bold">{`${props.user.nombre} ${props.user.apellidoP} ${props.user.apellidoM}`}</p>
+            <p className="manrope4">{`Operador #${props.user.numOperador}`}</p>
        </div>
     )
 }
