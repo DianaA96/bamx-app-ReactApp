93999e01936ff23daa9d68999cceb4a3a45dba0e
Diana Aguirre <a01276380@itesm.mx>
2021-10-05 20:49:01 -0500

    Actualiza estilos y jsx de card operador detalles

--- src/components/CardOperadorDetalles.js
+++ src/components/CardOperadorDetalles.js
@@ -7,39 +7,39 @@ import iconoPan from '../assets/icons/iconoPan.png'
 import iconoAbarrote from '../assets/icons/iconoAbarrote.png'
 import iconoNoComestible from '../assets/icons/iconoNoComestible.png'
 
-function CardOperadorDetalles() {
+function CardOperadorDetalles(props) {
     return (
        <div className="cardOperadorDetalles-container darkGlass">
            <section className="cardOperadorDetalles-1">
-               <p className="manrope4 folio-recoleccion bold">Folio  RE2456-B</p>
-                <p className="manrope5 bold">Rodrigo Hernandez B.</p>
-                <p className="manrope5">Operador #32123</p>
+               <p className="manrope4 folio-recoleccion bold">{`Folio ${props.user.folio}`}</p>
+                <p className="manrope5 bold">{`${props.user.nombre} ${props.user.apellidoP} ${props.user.apellidoM}`}</p>
+                <p className="manrope5">{`Operador #${props.user.numOperador}`}</p>
            </section>
            <section className="cardOperadorDetalles-2">
-                <p className="manrope5">Recibió de <span className="bold">Javier Almazán</span> en <span className="bold">Walmart Circunvalación</span>  Av. Circunvalación No. 4</p>
+                <p className="manrope5">Recibió de <span className="bold">{props.user.personaDonador}</span> en <span className="bold">{props.user.tiendaDonador}</span>{`  ${props.user.direccionDonador}`}</p>
                 <div className="cantidades-lista">
                     <div className="cantidad-elemento">
                         <img src={iconoFrutaVerdura} alt="" className="icono-cantidad" />
-                        <p className="manrope5 bold">15 kg</p>
+                        <p className="manrope5 bold">{`${props.user.notaFrutaYVerdura} kg`}</p>
                     </div>
                     <div className="cantidad-elemento">
                         <img src={iconoPan} alt="" className="icono-cantidad" />
-                        <p className="manrope5 bold">15 kg</p>
+                        <p className="manrope5 bold">{`${props.user.notaPan} kg`}</p>
                     </div>
                     <div className="cantidad-elemento">
                         <img src={iconoAbarrote} alt="" className="icono-cantidad" />
-                        <p className="manrope5 bold">15 kg</p>
+                        <p className="manrope5 bold">{`${props.user.notaAbarrote} kg`}</p>
                     </div>
                     <div className="cantidad-elemento">
                         <img src={iconoNoComestible} alt="" className="icono-cantidad" />
-                        <p className="manrope5 bold">15 kg</p>
+                        <p className="manrope5 bold">{`${props.user.notaNoComestible} kg`}</p>
                     </div>
                 </div>
            </section>
            <section className="cardOperadorDetalles-3">
-               <p className="manrope5">12 de septiembre del 2021</p>
-               <p className="manrope5 bold hora-recoleccion">14:35 Hrs.</p>
-               <p className="manrope5">NO recibió nota del donador.</p>
+               <p className="manrope5">{props.user.fechaRecepcionDonativo}</p>
+               <p className="manrope5 bold hora-recoleccion">{props.user.horaRecepcionDonativo}</p>
+               <p className="manrope5">{`${props.user.notaDelOperadorRecibida? "SI": "NO"} recibió nota del donador.`}</p>
            </section>
        </div>
     )
