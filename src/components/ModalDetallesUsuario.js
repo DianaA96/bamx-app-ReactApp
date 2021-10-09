4af88c504ebaada1984ae01f51048b8fda648ff5
EmmanuelBoM <a01276310@itesm.mx>
2021-10-08 20:19:13 -0500

    Funciona get de todos los usuarios, detalle de uusuario y eliminar usuario

--- src/components/ModalDetallesUsuario.js
+++ src/components/ModalDetallesUsuario.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import iconoUsuario from '../assets/icons/iconoUnidadDetalle.png'
 import '../styles/general.css';
 import '../styles/ModalDetallesRuta.css';
 import '../styles/ModalDetallesUsuario.css';
-import iconoUsuario from '../assets/icons/iconoUnidadDetalle.png'
 import '../styles/glass.css';
 import '../styles/botones.css';
+import axios from 'axios'
 import styled, { keyframes } from "styled-components";
 import { fadeInDownBig } from 'react-animations'
 
@@ -17,15 +18,9 @@ const BounceInDiv = styled.div`
 `;
 
 function ModalDetallesUsuario(props) {
-
-    let usuario = {
-        idUsuario: `${props.userId}`,
-        nombreUsuario: 'Daniel',
-        apellidosUsuario: 'Sánchez Cornejo',
-        rolUsuario: 'Operador',
-        correoUsuario: 'daniel@correofalso.com',
-        numeroTelfUsuario: '77 7234 5678' 
-    }
+    const [status, setStatus ] = useState('idle');
+    const [error, setError] = useState(null);
+    const [user, setUser] = useState({})
 
     function hideModal(){
         props.setModalVisibility(false)
@@ -36,46 +31,73 @@ function ModalDetallesUsuario(props) {
         props.setModalVisibility(false)
     }
 
-    return (
-        <div className="modal-overlay">
-            <BounceInDiv>
-                <div className="modalDetallesUsuario-container darkGlass">
-                    <section className="modalDetallesUsuario-izquierda">
-                        <h1 className="bebas1 blanco">Detalle del usuario</h1>
-                        <img src={iconoUsuario} alt="" />
-                    </section>
-                    <section className="modalDetallesUsuario-derecha">
-                        <section className="modalDetallesUsuario-header">
-                            <div className="modalTitulo">
-                                <p className="manrope5">ID</p>
-                                <p className="manrope5">{usuario.idUsuario}</p>
-                            </div>
-                            <button className="btn-cerrar" onClick={hideModal}><i class="fas fa-times-circle colorG100"></i></button>
+    useEffect(()=>{
+        setStatus('loading')
+        axios.get(`http://localhost:5000/users/${props.userId}`)
+          .then((result)=>{
+            setUser(result.data.datosUsuario[0])
+            console.log(user)
+            setStatus('resolved')
+          })
+          .catch((error)=>{
+            setError(error)
+            setStatus('error')
+          })
+    },[])
+
+    if(status === 'idle' || status === 'loading'){
+        return <p>Cargando</p>
+    }
+    
+    
+    if(status === 'error'){
+        return (
+            <p>{`${error.message} ${error.name}`}</p>
+        )
+    }
+    
+    if(status === 'resolved'){
+        return (
+            <div className="modal-overlay">
+                <BounceInDiv>
+                    <div className="modalDetallesUsuario-container darkGlass">
+                        <section className="modalDetallesUsuario-izquierda">
+                            <h1 className="bebas1 blanco">Detalle del usuario</h1>
+                            <img src={iconoUsuario} alt="" />
                         </section>
-                        <section className="modalDetallesUsuario-body">
-                            <div className="modalTitulo">
-                                <p className="manrope4">{usuario.nombreUsuario}</p>
-                                <p className="manrope4">{usuario.apellidosUsuario}</p>
-                                <p className="manrope5">{usuario.rolUsuario}</p>
-                            </div>
-                        </section>
-                        <section className="modalDetallesUsuario-body">
-                            <div className="modalUsuarioIndividual">
-                                <div className="infoUsuarioIndividual">
-                                    <p className="manrope5">{usuario.correoUsuario}</p>
-                                    <p className="manrope5">{usuario.numeroTelfUsuario}</p>
+                        <section className="modalDetallesUsuario-derecha">
+                            <section className="modalDetallesUsuario-header">
+                                <div className="modalTitulo">
+                                    <p className="manrope4">ID</p>
+                                    <p className="manrope5 bold">{user.nombreUsuario}</p>
                                 </div>
-                            </div>
+                                <button className="btn-cerrar" onClick={hideModal}><i class="fas fa-times-circle colorG100"></i></button>
+                            </section>
+                            <section className="modalDetallesUsuario-body">
+                                <div className="modalTitulo">
+                                    <p className="manrope4">{user.nombre}</p>
+                                    <p className="manrope4">{`${user.apellidoP} ${user.apellidoM}`}</p>
+                                    <p className="manrope5">{props.cargo}</p>
+                                </div>
+                            </section>
+                            <section className="modalDetallesUsuario-body">
+                                <div className="modalUsuarioIndividual">
+                                    <div className="infoUsuarioIndividual">
+                                        <p className="manrope5">{user.email}</p>
+                                        <p className="manrope5">{user.telefono}</p>
+                                    </div>
+                                </div>
+                            </section>
+                            <section className="modalDetallesUsuario-acciones">
+                                <button className="btnRosa bebas4" onClick={showModalConfirmacion}>Eliminar usuario</button>
+                                <button className="btnAmarillo bebas4">Editar datos del usuario</button>
+                            </section>
                         </section>
-                        <section className="modalDetallesUsuario-acciones">
-                            <button className="btnRosa bebas4" onClick={showModalConfirmacion}>Eliminar usuario</button>
-                            <button className="btnAmarillo bebas4">Editar datos del usuario</button>
-                        </section>
-                    </section>
-                </div>
-            </BounceInDiv>
-        </div>
-    )
+                    </div>
+                </BounceInDiv>
+            </div>
+        )
+    }
 }
 
 export default ModalDetallesUsuario
