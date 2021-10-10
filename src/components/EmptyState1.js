5b943d160144ff9f24596d38db10a50144a1168b
Diana Aguirre <a01276380@itesm.mx>
2021-10-10 14:24:30 -0500

    Empty State recibe props

--- src/components/EmptyState1.js
+++ src/components/EmptyState1.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Zanahorias from '../assets/images/searchingCarrots22.png'
 import '../styles/views.css'
 
-function EmptyState1() {
+function EmptyState1(props) {
     return(
         <>
             <div className="aguacateError">
                 <img className="zanahoriaVacia" src={Zanahorias} alt="¡Error!"/>
                 <p className="manrope4 blanco textoError">Ups.</p>
-                <p className="manrope4 blanco textoError">Aún no hay nada por aquí.</p>
+                <p className="manrope4 blanco textoError">{props.message}</p>
             </div>
         </>
     )
