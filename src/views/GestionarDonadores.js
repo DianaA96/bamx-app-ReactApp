7c4c3da5cfa4b547823ef818817496bf854e59ce
EmmanuelBoM <a01276310@itesm.mx>
2021-10-09 20:19:19 -0500

    Funciona GestionarDonadores

--- src/views/GestionarDonadores.js
+++ src/views/GestionarDonadores.js
@@ -4,8 +4,8 @@ import MenuPrincipal from '../components/MenuPrincipal'
 import CardCuadrada from '../components/CardCuadrada'
 import ModalDetallesDonador from '../components/ModalDetallesDonador'
 import ModalConfirmacion from '../components/ModalConfirmacion'
-
 import '../styles/views.css'
+import axios from 'axios' 
 
 function GestionarDonadores() {
 
@@ -13,61 +13,72 @@ function GestionarDonadores() {
     const [modalConfirmacionVisibility, setModalConfirmacionVisibility] = useState(false)
     const [ donorId, setDonorId ] = useState('');
     const cardType = "donador"
+    const [status, setStatus ] = useState('idle');
+    const [error, setError] = useState(null);
+    const [donors, setDonors] = useState([]);
 
-    const donors = [
-        {
-            idDonor:'1',
-            nombre:'Superama Gobernadores',
-            ubicacion:'Av Vicente Guerrero 760, Lomas de la Selva, 62270'
-        },
-        {
-            idDonor:'2',
-            nombre:'Fresko La Comer',
-            ubicacion:'Río Balsas 102, Vista Hermosa 62270'
-        },
-        {
-            idDonor:'3',
-            nombre:'Súper Chedraui',
-            ubicacion:'Blvd. Gustavo Díaz Ordaz, Cantarranas, 62448'
-        },
-        {
-            idDonor:'4',
-            nombre:'Súper Chedraui',
-            ubicacion:'Blvd. Gustavo Díaz Ordaz, Cantarranas, 62448'
-        },
-        {
-            idDonor:'5',
-            nombre:'Súper Chedraui',
-            ubicacion:'Blvd. Gustavo Díaz Ordaz, Cantarranas, 62448'
-        },
-        {
-            idDonor:'6',
-            nombre:'Súper Chedraui',
-            ubicacion:'Blvd. Gustavo Díaz Ordaz, Cantarranas, 62448'
-        }
-    ]
+    useEffect(()=>{
+        setStatus('loading')
+        axios.get(`http://localhost:5000/donors`)
+          .then((result)=>{
+              console.log(result)
+            setDonors(result.data.listaDonadores)
+            setStatus('resolved')
+          })
+          .catch((error)=>{
+            setError(error)
+            setStatus('error')
+          })
+    },[])
 
-    return (
-        <body className="green-gradient">
-            <aside>
-                <MenuPrincipal></MenuPrincipal>
-            </aside>
-            <main>
-                <header>
-                    <HeaderBusquedaDonadores></HeaderBusquedaDonadores>
-                </header>
-                <section className="contenido">
-                    <div className="cardsCuadradas-container">
-                        {donors.map((data,i)=>
-                            <CardCuadrada cardType={cardType} data = {data} setModalVisibility={setModalVisibility} setModalConfirmacionVisibility={setModalConfirmacionVisibility} setDonorId={setDonorId}></CardCuadrada>
-                        )}
-                    </div>
-                </section>
-                {modalVisibility ? <ModalDetallesDonador donorId={donorId} setModalConfirmacionVisibility={setModalConfirmacionVisibility} setModalVisibility={setModalVisibility}></ModalDetallesDonador>  : null}
-                {modalConfirmacionVisibility ? <ModalConfirmacion  setModalConfirmacionVisibility={setModalConfirmacionVisibility} titulo1="eliminación" titulo2="donador" accion="eliminar" entidadObjetivo=" el donador" idEntidad={donorId}></ModalConfirmacion>:null}
-            </main>
-        </body>
-    )
+    
+    function handleDelete(){
+        axios.delete(`http://localhost:5000/donors/${donorId}`)
+        .then((result)=>{
+            setStatus('resolved')
+            alert("Donador " + result.data.usuarioEliminado.nombreUsuario + " deshabilitado")
+            setModalConfirmacionVisibility(false);
+        })
+        .catch((error)=>{
+            setError(error)
+            setStatus('error')
+        })
+    }
+
+    if(status === 'idle' || status === 'loading'){
+        return <p>Cargando</p>
+    }
+    
+    
+    if(status === 'error'){
+        return (
+            <p>{`${error.message} ${error.name}`}</p>
+        )
+    }
+    
+    if(status === 'resolved'){
+        return (
+            <body className="green-gradient">
+                <aside>
+                    <MenuPrincipal></MenuPrincipal>
+                </aside>
+                <main>
+                    <header>
+                        <HeaderBusquedaDonadores></HeaderBusquedaDonadores>
+                    </header>
+                    <section className="contenido">
+                        <div className="cardsCuadradas-container">
+                            {donors.map((data,i)=>
+                                <CardCuadrada cardType={cardType} data = {data} setModalVisibility={setModalVisibility} setModalConfirmacionVisibility={setModalConfirmacionVisibility} setDonorId={setDonorId}></CardCuadrada>
+                            )}
+                        </div>
+                    </section>
+                    {modalVisibility ? <ModalDetallesDonador donorId={donorId} setModalConfirmacionVisibility={setModalConfirmacionVisibility} setModalVisibility={setModalVisibility}></ModalDetallesDonador>  : null}
+                    {modalConfirmacionVisibility ? <ModalConfirmacion  setModalConfirmacionVisibility={setModalConfirmacionVisibility} titulo1="eliminación" titulo2="donador" accion="eliminar" entidadObjetivo=" el donador" idEntidad={donorId}></ModalConfirmacion>:null}
+                </main>
+            </body>
+        )
+    }
 }
 
 export default GestionarDonadores
